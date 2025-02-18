13c88adbb1bf70e93ba52a8ad0b2a962178c0376
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-18 21:00:33 +0200

    Add project files.

---
+++ Assignment1_FrameWork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1_FrameWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
