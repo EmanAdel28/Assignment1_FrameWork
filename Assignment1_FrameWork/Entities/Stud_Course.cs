cf2a6d796a3b87374c84404e6f1d54b99265ac68
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-18 21:21:57 +0200

    Create Stud_Curse Class

---
+++ Assignment1_FrameWork/Entities/Stud_Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_FrameWork.Entities
+{
+    internal class Stud_Course
+    {
+        public int stud_ID { get; set; }
+        public int Course_ID { get; set; }
+
+        [Range(0,4)]
+        public decimal Grade { get; set; }
+    }
+}
